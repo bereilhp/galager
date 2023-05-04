fc2ae3da328153932aacd586be4c9d5d086e6856
PedroB <bereilhp@gmail.com>
2023-05-04 10:23:16 +0200

    new modules.css

--- my-app/src/pages/login.js
+++ my-app/src/pages/login.js
@@ -1,27 +1,5 @@
-import Head from "next/head";
+import Login from "../components/login";
 
-export default function Login() {
-  return (
-    <div>
-      <Head>
-        <title>Login Page</title>
-        <link rel="icon" href="/galager.ico" />
-      </Head>
-      <div className="bg-slate-800 flex items-center justify-center h-screen">
-        <form>
-          <div className="mb-4">
-            <label className="text-white text-2xl">Email:</label>
-            <input className="w-full px-4 py-2 border rounded-lg text-green-500" type="email" required />
-          </div>
-          <div className="mb-8">
-            <label className="text-white text-2xl">Password:</label>
-            <input className="w-full px-4 py-2 border rounded-lg text-green-500" type="password" required />
-          </div>
-          <button className="text-2xl w-full px-4 py-2 text-black bg-blue-500 rounded hover:bg-sky-700" type="submit">
-            Submit
-          </button>
-        </form>
-      </div>
-    </div>
-  );
+export default function LoginPage() {
+  return <Login />;
 }
