fac370361be96c8e162f376a1bfc393718fbc404
PedroB <bereilhp@gmail.com>
2023-05-11 21:15:24 +0200

    Update pkg and fix tailwind.config

--- my-app/tailwind.config.js
+++ my-app/tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.js',
-    './src/components/**/*.jsx',
+    './src/pages/*.js',
+    './src/components/*.jsx',
   ],
   theme: {
     extend: {
