ba555f647a47169bf017d00f28aefac8037f7af3
PedroB <bereilhp@gmail.com>
2023-06-13 22:42:58 +0200

    Fix problems with sign-in and up

--- my-app/app/sign-in/[[...sign-in]]/page.js
+++ my-app/app/sign-in/[[...sign-in]]/page.js
@@ -4,7 +4,8 @@ import style from "./signin.module.css"
 export default function Page() {
   return (
     <div className={style.center}>
-      <SignIn />
+      <SignIn path="/sign-in" routing="path" signUpUrl="/sign-up" />
     </div>
   )
 }
+
