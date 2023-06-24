40820407bc474125587b94e7fa5fb835375cee4e
PedroB <bereilhp@gmail.com>
2023-06-24 11:12:48 +0200

    Navbar

---
+++ my-app/app/contact/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: 'Sign-up',
+  description: '',
+}
+
+export default function ContactLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
--- my-app/app/page.js
+++ my-app/app/page.js
@@ -1,9 +1,16 @@
 import { UserButton } from "@clerk/nextjs";
 import style from "./page.module.css"
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div>
+      <ul>
+        <li><Link href="/">Home</Link></li>
+        <li><Link href="/">Learn</Link></li>
+        <li><Link href="/">Exercises</Link></li>
+        <li><Link href="/contact">Contact</Link></li>
+      </ul>
       <div className={style.right}>
         <UserButton></UserButton>
       </div>
