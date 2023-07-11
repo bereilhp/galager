aa5e0136c1c58bb58de316cdedf5eca413ce0f3d
PedroB <bereilhp@gmail.com>
2023-07-11 15:16:36 +0200

    Navbar Center

--- my-app/app/_components/_navbar/Navbar.jsx
+++ my-app/app/_components/_navbar/Navbar.jsx
@@ -1,12 +1,19 @@
 import { UserButton } from "@clerk/nextjs";
 import Link from "next/link";
 import style from "./navbar.module.css";
+import Image from "next/image";
 
 export default function Navbar() {
   return (
     <div>
       <div className={style.navbar}>
         <nav className={style.navbarLinks}>
+          <Image src="/_img/favicon.ico"
+                    width={30}
+                    height={30}
+                    alt="Galager Logo"
+                />
+          <div className={style.pad}></div>
           <Link href="/">Home</Link>
           <Link href="/learn">Learn</Link>
           <Link href="exercises">Exercises</Link>
