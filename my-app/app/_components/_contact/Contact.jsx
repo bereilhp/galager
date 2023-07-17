4922997e43e9ff8904b7c90fb3204bdc4fb3bc4f
PedroB <bereilhp@gmail.com>
2023-07-17 19:33:52 +0200

    Contact Finished

--- my-app/app/_components/_contact/Contact.jsx
+++ my-app/app/_components/_contact/Contact.jsx
@@ -1,13 +1,29 @@
+'use client'
+
 import Image from "next/image";
 import style from "./contact.module.css"
 
 export default function Contact() {
     return (
         <div>
-            <h1 className={style.center}>
-                Contact Us
-            </h1>
-            <p className={style.text}> For any inquiries or feedback, please feel free to reach out to us at <strong>galagerOfficial@proton.me</strong></p>
+            <div className={style.center}>
+                <Image
+                    src="/_img/logo.png"
+                    width={800}
+                    height={225}
+                    alt="Galager Logo"
+                />
+            </div>
+            <p className={style.text}> For any inquiries or feedback, please feel free to reach out to us at <strong className={style.mail}>galagerOfficial@proton.me</strong></p>
+            <div className={style.padding}></div>
+            <div className={style.center}>
+                <Image
+                    src="/_img/homer.gif"
+                    width={600}
+                    height={300}
+                    alt="Galager Logo"
+                ></Image>
+            </div>
         </div>
     );
 };
