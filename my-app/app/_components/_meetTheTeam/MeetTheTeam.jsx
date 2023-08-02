07dcca4891b58f798872b2fa67281f64c6918180
PedroB <bereilhp@gmail.com>
2023-08-02 13:44:57 +0200

    Meet the team

--- my-app/app/_components/_meetTheTeam/MeetTheTeam.jsx
+++ my-app/app/_components/_meetTheTeam/MeetTheTeam.jsx
@@ -4,8 +4,9 @@ export default function MeetTheTeam() {
     return (
         <div>
             <div className={style.title}>Meet the Team</div>
-            <div>
-                Meet our dedicated team of passionate developers. Well, truth be told, it's currently just one—yours truly! But hey, I'm all-in on building an e-learning platform that embraces diversity and inclusion. Programming has truly changed my life, opening a whole new world of sleepless nights, stress, and let's not forget the infamous ritual of copying and pasting an error into Google and praying that someone from 8 years ago had the same problem! Yet, despite all the challenges, programming is nothing short of amazing.
+            <br />
+            <div className={style.text}>
+                Meet our dedicated team of passionate developers. Well, truth be told, it's currently just one, yours truly! But hey, I'm all-in on building an e-learning platform that embraces diversity and inclusion. Programming has truly changed my life, opening a whole new world of sleepless nights, stress, and let's not forget the infamous ritual of copying and pasting an error into Google and praying that someone from 8 years ago had the same problem. Yet, despite all the challenges, programming is nothing short of amazing.
                 <br /><br />
                 I can't contain my excitement about creating <span className={style.green}>Galager.</span> A place where aspiring developers can find support, inspiration, and most importantly, enjoy the journey.
                 <br /><br />
