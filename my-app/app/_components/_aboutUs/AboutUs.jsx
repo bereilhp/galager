31bb22537a9894d7ae571560dbc25ed9de47a5f1
PedroB <bereilhp@gmail.com>
2023-08-02 13:46:56 +0200

    About us

--- my-app/app/_components/_aboutUs/AboutUs.jsx
+++ my-app/app/_components/_aboutUs/AboutUs.jsx
@@ -4,7 +4,7 @@ export default function AboutUs() {
     return (
         <div>
             <div className={style.title}>About Us</div>
-            <div>
+            <div className={style.text}>
                 <span className={style.green}>Galager</span> is a web app dedicated to breaking barriers and creating opportunities for individuals interested in programming, regardless of their background or abilities.
                 <br /><br />
                 We believe in the power of technology to transform lives, and by offering free and open-source resources, we aim to enable aspiring developers from all around the world to discover their true capabilities through programming. We are creating a community that thrives on mutual support and collaboration, fostering an environment where everyone can grow and succeed in the world of software development.
