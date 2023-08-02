c0f20544971c7f7f1bc0d4cc2df8c5e8d006f2a1
PedroB <bereilhp@gmail.com>
2023-08-02 13:45:56 +0200

    Our Mission

--- my-app/app/_components/_ourMission/OurMission.jsx
+++ my-app/app/_components/_ourMission/OurMission.jsx
@@ -4,7 +4,7 @@ export default function OurMission() {
     return (
         <div>
             <div className={style.title}>Our Mission</div>
-            <div>
+            <div className={style.text}>
                 At <span className={style.green}>Galager</span>, our mission is to empower individuals of all abilities to learn coding.
             </div>
         </div>
