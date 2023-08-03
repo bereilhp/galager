cfafbf53dbd64c924ea57046db421ef6901d35b8
PedroB <bereilhp@gmail.com>
2023-08-03 20:35:37 +0200

    Chapter 0 Learn

---
+++ my-app/app/learn/chapter0/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: 'Chapter 0',
+  description: '',
+}
+
+export default function Chapter0Layout({ children }) {
+  return (
+    <>
+      {children}
+    </>
+  )
+}
---
+++ my-app/app/learn/chapter0/page.js
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <div>
+      <h1>Chapter 0</h1>
+    </div>
+  )
+}
