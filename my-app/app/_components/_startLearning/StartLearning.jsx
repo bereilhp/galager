4da26d37bdcff1773699f4f65bde1ad3a7727399
PedroB <bereilhp@gmail.com>
2023-08-05 20:36:18 +0200

    Start Learning Component

---
+++ my-app/app/_components/_startLearning/StartLearning.jsx
@@ -0,0 +1,10 @@
+import style from "./startLearning.module.css";
+
+
+export default function StartLearning() {
+    return (
+        <div>
+            <button>Hello</button>
+        </div>
+    )
+}
--- my-app/app/learn/page.js
+++ my-app/app/learn/page.js
@@ -1,10 +1,12 @@
 import Footer from "../_components/_footer/Footer";
 import Navbar from "../_components/_navbar/Navbar";
+import StartLearning from "../_components/_startLearning/StartLearning";
 
 export default function Page() {
   return (
     <div>
       <Navbar/>
+      <StartLearning></StartLearning>
       <h1>Soy Learn</h1>
       <Footer/>
     </div>
