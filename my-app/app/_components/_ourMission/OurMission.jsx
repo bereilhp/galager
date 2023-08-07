b84a52fe1615fb107019b02629afde6693c0dc0b
PedroB <bereilhp@gmail.com>
2023-08-07 17:53:04 +0200

    Arrow svg image Our Mission

--- my-app/app/_components/_ourMission/OurMission.jsx
+++ my-app/app/_components/_ourMission/OurMission.jsx
@@ -1,8 +1,15 @@
+import Image from "next/image";
 import style from "./ourMission.module.css";
 
 export default function OurMission() {
     return (
         <div>
+            <Image
+                    src="/_img/ourMission.svg"
+                    width={200}
+                    height={225}
+                    alt="Arrow in the center of a target"
+                />
             <div className={style.title}>Our Mission</div>
             <div className={style.text}>
                 At <span className={style.green}>Galager</span>, our mission is to empower individuals of all abilities to learn coding.
