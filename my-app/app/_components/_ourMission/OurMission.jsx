fafb0865cea287cc26be6fc96f109555f0353330
PedroB <bereilhp@gmail.com>
2023-08-07 18:06:26 +0200

    Our mission

--- my-app/app/_components/_ourMission/OurMission.jsx
+++ my-app/app/_components/_ourMission/OurMission.jsx
@@ -1,19 +1,28 @@
+import React from "react";
 import Image from "next/image";
 import style from "./ourMission.module.css";
 
 export default function OurMission() {
-    return (
-        <div>
-            <Image
-                    src="/_img/ourMission.svg"
-                    width={200}
-                    height={225}
-                    alt="Arrow in the center of a target"
-                />
-            <div className={style.title}>Our Mission</div>
-            <div className={style.text}>
-                At <span className={style.green}>Galager</span>, our mission is to empower individuals of all abilities to learn coding.
-            </div>
+  return (
+    <div className={style.center}>
+      <div className={style.missionContainer}>
+        <div className={style.imageContainer}>
+          <Image
+            src="/_img/ourMission.svg"
+            width={200}
+            height={225}
+            alt="Arrow in the center of a target"
+            className={style.image}
+          />
         </div>
-    )
+        <div className={style.textContainer}>
+          <div className={style.title}>Our Mission</div>
+          <div className={style.blueLine}></div>
+          <div className={style.text}>
+            At <span className={style.galager}>Galager</span>, our mission is to empower individuals of all abilities to learn coding.
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
