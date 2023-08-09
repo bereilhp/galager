c9dc4b31c84124ec83c624e58b0350461dc73d61
PedroB <bereilhp@gmail.com>
2023-08-09 19:18:14 +0200

    Chapter 0 main idea

--- my-app/app/learn/chapter0/page.js
+++ my-app/app/learn/chapter0/page.js
@@ -1,7 +1,12 @@
+import FooterFixed from "@/app/_components/_footerFixed/FooterFixed";
+import Navbar from "@/app/_components/_navbar/Navbar";
+
 export default function Page() {
   return (
     <div>
+      <Navbar></Navbar>
       <h1>Chapter 0</h1>
+      <FooterFixed></FooterFixed>
     </div>
   )
 }
