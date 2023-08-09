cb5522a53503d1cfd4a7882ef7bd32300d1e5974
PedroB <bereilhp@gmail.com>
2023-08-09 19:46:29 +0200

    Chapter 2 page

---
+++ my-app/app/learn/chapter2/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: 'Chapter 2',
+  description: '',
+}
+
+export default function Chapter2Layout({ children }) {
+  return (
+    <>
+      {children}
+    </>
+  )
+}
