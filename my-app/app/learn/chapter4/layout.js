07d461f9b094f37794f5e6c52ff5c385fd4a259a
PedroB <bereilhp@gmail.com>
2023-08-09 19:47:36 +0200

    Chapter 4 page

---
+++ my-app/app/learn/chapter4/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: 'Chapter 4',
+  description: '',
+}
+
+export default function Chapter4Layout({ children }) {
+  return (
+    <>
+      {children}
+    </>
+  )
+}
---
+++ my-app/app/learn/chapter4/page.js
@@ -0,0 +1,14 @@
+import FooterFixed from "@/app/_components/_footerFixed/FooterFixed";
+import Navbar from "@/app/_components/_navbar/Navbar";
+import NextLesson from "@/app/_components/_nextLesson/NextLesson";
+
+export default function Page() {
+  return (
+    <div>
+      <Navbar></Navbar>
+      <h1>Chapter 4</h1>
+      <NextLesson></NextLesson>
+      <FooterFixed></FooterFixed>
+    </div>
+  )
+}
