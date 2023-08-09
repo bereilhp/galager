ee748ce40995226f9b273cabcd2d72bb8050d574
PedroB <bereilhp@gmail.com>
2023-08-09 19:58:24 +0200

    Next Lesson Component quick css fix

--- my-app/app/_components/_nextLesson/NextLesson.jsx
+++ my-app/app/_components/_nextLesson/NextLesson.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import styles from "./nextLesson.module.css";
+import Link from "next/link";
 
 export default function NextLesson() {
   return (
     <div className={styles.lessonButtons}>
-      <button className={styles.backButton}>Previous Chapter</button>
-      <button className={styles.nextButton}>Next Chapter</button>
+      <Link href="/learn/chapter0" className={styles.backButton}>Previous Chapter</Link>
+      <Link href="/learn/chapter0" className={styles.nextButton}>Next Chapter</Link>
     </div>
   );
 }
