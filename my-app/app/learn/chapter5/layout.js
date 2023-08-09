248f9c41a2572dd26e35fd8c901e8bfe6c6a4b97
PedroB <bereilhp@gmail.com>
2023-08-09 19:48:22 +0200

    Chapter 5 page

---
+++ my-app/app/learn/chapter5/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: 'Chapter 5',
+  description: '',
+}
+
+export default function Chapter5Layout({ children }) {
+  return (
+    <>
+      {children}
+    </>
+  )
+}
