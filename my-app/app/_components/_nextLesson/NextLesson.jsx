da6af7681631b8befc2ed552f09ec150297c3cab
PedroB <bereilhp@gmail.com>
2023-08-09 19:44:11 +0200

    Previous and Next chapter component with css

--- my-app/app/_components/_nextLesson/NextLesson.jsx
+++ my-app/app/_components/_nextLesson/NextLesson.jsx
@@ -5,7 +5,7 @@ export default function NextLesson() {
   return (
     <div className={styles.lessonButtons}>
       <button className={styles.backButton}>Previous Chapter</button>
-      <button className={styles.nextButton}>Next Lesson</button>
+      <button className={styles.nextButton}>Next Chapter</button>
     </div>
   );
 }
