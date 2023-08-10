e884a410b9090d9d4c3a4f566c661f8847cb2c85
PedroB <bereilhp@gmail.com>
2023-08-10 19:49:10 +0200

    Learn page

--- my-app/app/learn/page.js
+++ my-app/app/learn/page.js
@@ -1,13 +1,15 @@
 import Footer from "../_components/_footer/Footer";
 import Navbar from "../_components/_navbar/Navbar";
+import Pad from "../_components/_pad/Pad";
 import StartLearning from "../_components/_startLearning/StartLearning";
 
 export default function Page() {
   return (
     <div>
       <Navbar/>
-      <StartLearning></StartLearning>
       <h1>Soy Learn</h1>
+      <StartLearning></StartLearning>
+      <Pad></Pad>
       <Footer/>
     </div>
   )
