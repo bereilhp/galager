49a8f9ea160a4e6ef0284d10e71083744dc6e545
PedroB <bereilhp@gmail.com>
2023-08-10 20:11:28 +0200

    Start Learning

---
+++ my-app/app/_components/_learnIntro/LearnIntro.jsx
@@ -0,0 +1,25 @@
+import Image from "next/image";
+import StartLearning from "../_startLearning/StartLearning";
+import style from "./learnIntro.module.css";
+
+export default function LearnIntro() {
+    return (
+        <div className={style.introContainer}>
+            <div className={style.introContent}>
+                <h1 className={style.introHeading}>Welcome to the JavaScript Learning Course</h1>
+                <p className={style.introDescription}>
+                    Explore the exciting world of programming and JavaScript in a fun and interactive way!
+                </p>
+                <StartLearning></StartLearning>
+            </div>
+            <div className={style.introImage}>
+                <Image
+                    src="/_img/JavaScript-logo.png"
+                    width={225}
+                    height={225}
+                    alt="JavaScript Logo"
+                />
+            </div>
+        </div>
+    );
+}
--- my-app/app/learn/page.js
+++ my-app/app/learn/page.js
@@ -1,16 +1,15 @@
 import Footer from "../_components/_footer/Footer";
+import FooterFixed from "../_components/_footerFixed/FooterFixed";
+import LearnIntro from "../_components/_learnIntro/LearnIntro";
 import Navbar from "../_components/_navbar/Navbar";
 import Pad from "../_components/_pad/Pad";
-import StartLearning from "../_components/_startLearning/StartLearning";
 
 export default function Page() {
   return (
     <div>
       <Navbar/>
-      <h1>Soy Learn</h1>
-      <StartLearning></StartLearning>
-      <Pad></Pad>
-      <Footer/>
+      <LearnIntro></LearnIntro>
+      <FooterFixed></FooterFixed>
     </div>
   )
 }
