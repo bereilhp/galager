3987cd8469627e69e2d689e64e4fa00f3727ed1b
PedroB <bereilhp@gmail.com>
2023-08-11 12:57:46 +0200

    route signup

---
+++ my-app/src/app/api/users/signup/route.js
@@ -0,0 +1,44 @@
+import { connect } from "@/src/db/config.js";
+import User from "@/src/models/userModel.js";
+import { NextRequest, NextResponse } from "next/server";
+import bcryptjs from "bcryptjs";
+
+
+connect();
+
+export async function POST(req, res) {
+    try {
+        const reqBody = await req.json()
+        const { username, email, password } = reqBody;
+
+        console.log(reqBody);
+
+        //Check if user already exists
+        const user = await User.findOne({ email });
+
+        if (user) {
+            return NextResponse.json({ error: "User already exists" }, { status: 400 })
+        }
+
+        //hash password
+        const salt = await bcryptjs.genSalt(10);
+        const hashedPassword = await bcryptjs.hash(password, salt);
+
+        //new user to be saved in db 
+        const newUser = new User({
+            username,
+            email,
+            password: hashedPassword
+        });
+
+        const savedUser = await newUser.save();
+        console.log(savedUser);
+
+        return NextResponse.json({ message: "User created succesfully", success: true, savedUser });
+
+
+    } catch (error) {
+        return NextResponse.json({ error: error.message }, { status: 500 })
+
+    }
+}
