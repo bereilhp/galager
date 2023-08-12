32e97b5145cee809e0d8483597d256d5b270fe37
PedroB <bereilhp@gmail.com>
2023-08-12 19:00:36 +0200

    signup layout

--- my-app/src/app/login/layout.js
+++ my-app/src/app/login/layout.js
@@ -1,12 +1,12 @@
 export const metadata = {
-    title: 'Login',
-    description: '',
-  }
-  
-  export default function LoginLayout({ children }) {
-    return (
-      <>
-        {children}
-      </>
-    )
-  }
+  title: 'Login',
+  description: '',
+}
+
+export default function LoginLayout({ children }) {
+  return (
+    <>
+      {children}
+    </>
+  )
+}
---
+++ my-app/src/app/signup/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+    title: 'Signup',
+    description: '',
+}
+
+export default function SignupLayout({ children }) {
+    return (
+        <>
+            {children}
+        </>
+    )
+}
