858c48fe72654a5684d03f59c212203f91bf471f
PedroB <bereilhp@gmail.com>
2023-10-05 20:43:35 +0200

    Course Intro Component

--- my-app/src/app/courses/page.js
+++ my-app/src/app/courses/page.js
@@ -1,3 +1,4 @@
+import CoursesIntro from "../_components/_coursesIntro/CoursesIntro";
 import Footer from "../_components/_footer/Footer";
 import Navbar from "../_components/_navbar/Navbar";
 import Pad from "../_components/_pad/Pad";
@@ -6,11 +7,10 @@ import StartLearningJS from "../_components/_startLearninJS/StartLearning";
 export default function Page() {
   return (
     <div>
-      <Navbar/>
-      <h1>Soy Courses</h1>
-      <StartLearningJS></StartLearningJS>
+      <Navbar />
+      <CoursesIntro></CoursesIntro>
       <Pad></Pad>
-      <Footer/>
+      <Footer />
     </div>
-  )
+  );
 }
