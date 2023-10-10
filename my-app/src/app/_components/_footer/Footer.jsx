9a472ed70e1ac53638ba26717ce1a8215471524d
PedroB <bereilhp@gmail.com>
2023-10-10 17:03:49 +0200

    Footer and Navbar profile page

--- my-app/src/app/_components/_footer/Footer.jsx
+++ my-app/src/app/_components/_footer/Footer.jsx
@@ -1,22 +1,20 @@
-import style from "./footer.module.css"
+import style from "./footer.module.css";
 import Image from "next/image";
 
-export default function FooterFixed() {
-    return (
-        <footer className={style.footer}>
-            <div className={style.pad}>
-                <div className={style.copyright}>Copyright © 2023 Galager.</div>
-                <a
-                    href="https://github.com/bereilhp/galager"
-                    target="_blank"
-                >
-                    <Image
-                        src="/_img/github-mark/github-mark.png"
-                        width={40}
-                        height={40}
-                        alt="GitHub Logo"></Image>
-                </a>
-            </div>
-        </footer >
-    );
-};
+export default function Footer() {
+  return (
+    <footer className={style.footer}>
+      <div className={style.pad}>
+        <div className={style.copyright}>Copyright © 2023 Galager.</div>
+        <a href="https://github.com/bereilhp/galager" target="_blank">
+          <Image
+            src="/_img/github-mark/github-mark.png"
+            width={40}
+            height={40}
+            alt="GitHub Logo"
+          ></Image>
+        </a>
+      </div>
+    </footer>
+  );
+}
