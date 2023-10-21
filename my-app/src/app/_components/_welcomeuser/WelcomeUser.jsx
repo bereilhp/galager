7659e230efbc69e53b5ddc7e18d0d37106ed9091
PedroB <bereilhp@gmail.com>
2023-10-21 18:45:32 +0200

    Quiz List component with css

--- my-app/src/app/_components/_welcomeuser/WelcomeUser.jsx
+++ my-app/src/app/_components/_welcomeuser/WelcomeUser.jsx
@@ -1,9 +1,32 @@
+import Image from "next/image";
+import style from "./welcomeUser.module.css";
+import GetUsername from "../_getUsername/GetUsername";
 import Link from "next/link";
 
-export default function StartLearningJS() {
-    return (
-        <div>
-            <h1>Welcome User</h1>
+export default function WelcomeUser() {
+  return (
+    <div className={style.heroSection}>
+      <div className={style.heroSectionWrapper}>
+        <div className={style.overlapWrapper}>
+          <div className={style.overlap}>
+            <Image
+              src="/_img/JSLogoRectangle.png"
+              width={180}
+              height={180}
+              alt="JavaScript Logo"
+              className={style.jsLogo}
+            />
+            <GetUsername className={style.textWrapper}></GetUsername>
+            <p className={style.div}>
+              Galager is a free and open-source web app dedicated to learning
+              JavaScript.
+            </p>
+            <Link href="/courses/" className={style.button}>
+              Start Learning
+            </Link>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
