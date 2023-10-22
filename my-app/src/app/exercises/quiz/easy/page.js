1f59012597e43279b4ecf71f67bae3e91b96b346
PedroB <bereilhp@gmail.com>
2023-10-22 20:23:59 +0200

    Fix Css QuizList

--- my-app/src/app/exercises/quiz/easy/page.js
+++ my-app/src/app/exercises/quiz/easy/page.js
@@ -1,6 +1,7 @@
 import QuizBasicJS from "../../../_components/_quiz/QuizBasicJS";
 import Footer from "../../../_components/_footer/Footer";
 import Navbar from "../../../_components/_navbar/Navbar";
+import Pad from "@/src/app/_components/_pad/Pad";
 
 export default function EasyQuizPage() {
   return (
--- my-app/src/app/exercises/quiz/page.js
+++ my-app/src/app/exercises/quiz/page.js
@@ -2,12 +2,15 @@ import QuizBasicJS from "../../_components/_quiz/QuizBasicJS";
 import Footer from "../../_components/_footer/Footer";
 import Navbar from "../../_components/_navbar/Navbar";
 import QuizList from "../../_components/_quizList/QuizList";
+import Pad from "../../_components/_pad/Pad";
 
 export default function QuizPage() {
   return (
     <div>
       <Navbar />
       <QuizList></QuizList>
+      <Pad></Pad>
+      <Pad></Pad>
       <Footer />
     </div>
   );
