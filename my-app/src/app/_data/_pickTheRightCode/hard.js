aa8429f5c4e8c122f392d4c72ba728ec98e3cf51
PedroB <bereilhp@gmail.com>
2023-11-03 16:55:28 +0100

    Pick the Right Code Hard done

---
+++ my-app/src/app/_data/_pickTheRightCode/hard.js
@@ -0,0 +1,50 @@
+export const pickTheRightCode = {
+  totalQuestions: 5,
+  questions: [
+    {
+      id: 1,
+      question: "Which of these returns the factorial of 5?",
+      answers: [
+        "/_img/pickTheRightCode/hard/factorialBad.png",
+        "/_img/pickTheRightCode/hard/factorialGood.png",
+      ],
+      correctAnswer: "/_img/pickTheRightCode/hard/factorialGood.png",
+    },
+    {
+      id: 2,
+      question: "Which of these two code snippets returns 15?",
+      answers: [
+        "/_img/pickTheRightCode/hard/sumUpToGood.png",
+        "/_img/pickTheRightCode/hard/sumUpToBad.png",
+      ],
+      correctAnswer: "/_img/pickTheRightCode/hard/sumUpToGood.png",
+    },
+    {
+      id: 3,
+      question: "Which of these returns 'Elon Musk'?",
+      answers: [
+        "/_img/pickTheRightCode/hard/objGood.png",
+        "/_img/pickTheRightCode/hard/objBad.png",
+      ],
+      correctAnswer: "/_img/pickTheRightCode/hard/objGood.png",
+    },
+    {
+      id: 4,
+      question: "Which of these prints 'Linus Torvalds'?",
+      answers: [
+        "/_img/pickTheRightCode/hard/conditionBad.png",
+        "/_img/pickTheRightCode/hard/conditionGood.png",
+      ],
+      correctAnswer: "/_img/pickTheRightCode/hard/conditionGood.png",
+    },
+    {
+      id: 5,
+      question: "Which of these prints 'The value is a string.'?",
+      answers: [
+        "/_img/pickTheRightCode/hard/typeofBad.png",
+        "/_img/pickTheRightCode/hard/typeofGood.png",
+      ],
+      correctAnswer: "/_img/pickTheRightCode/hard/typeofGood.png",
+    },
+  ],
+};
