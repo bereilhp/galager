b05e311026760718f268ee85be5aa97b7903c072
PedroB <bereilhp@gmail.com>
2023-11-09 18:54:11 +0100

    Chapter 0 Component

--- my-app/src/app/courses/javascript/chapter0/page.js
+++ my-app/src/app/courses/javascript/chapter0/page.js
@@ -2,6 +2,7 @@ import Chapter0 from "@/src/app/_components/_chapter0/Chapter0";
 import Footer from "@/src/app/_components/_footer/Footer";
 import Navbar from "@/src/app/_components/_navbar/Navbar";
 import NextLesson from "@/src/app/_components/_nextLesson/NextLesson";
+import Pad from "@/src/app/_components/_pad/Pad";
 
 export default function Page() {
   const currentChapter = 0;
@@ -10,6 +11,7 @@ export default function Page() {
       <Navbar></Navbar>
       <Chapter0></Chapter0>
       <NextLesson currentChapter={currentChapter}></NextLesson>
+      <Pad></Pad>
       <Footer></Footer>
     </div>
   );
