3602e72fdcd1b76b93b7dfa81b1faf8244cb5acc
PedroB <bereilhp@gmail.com>
2023-11-11 19:19:00 +0100

    Chapter 4 Component

---
+++ my-app/src/app/_components/_chapter4/Chapter4.jsx
@@ -0,0 +1,80 @@
+import Image from "next/image";
+import style from "./chapter4.module.css";
+import Link from "next/link";
+
+export default function Chapter4() {
+  return (
+    <div>
+      <h1 className={style.title}>Chapter 4 - JavaScript</h1>
+      <div className={style.content}>
+        <div className={style.text}>
+          <section className={style.section}>
+            <h2>Node.js</h2>
+            <p>
+              Node.js is a runtime environment that allows you to run JavaScript
+              on the server-side. It uses the V8 JavaScript engine from Google
+              Chrome to execute code. Node.js is commonly used for building
+              scalable network applications and is a key component in modern web
+              development.
+            </p>
+            <div className={style.centeredImage}>
+              <Image
+                src="/_img/chapter4/nodejs.png"
+                alt="Node.js logo"
+                width={300}
+                height={200}
+              />
+            </div>
+          </section>
+
+          <section className={style.section}>
+            <h2>Running JavaScript in the Browser</h2>
+            <p>
+              JavaScript can be executed in the browser using the{" "}
+              <code>script</code> tag. You can include the script directly in
+              your HTML file or link to an external JavaScript file.
+            </p>
+            <div className={style.centeredImage}>
+              <Image
+                src="/_img/chapter4/html.png"
+                alt="Script tag example"
+                width={700}
+                height={600}
+              />
+            </div>
+          </section>
+
+          <section className={style.section}>
+            <h2>Frameworks and Libraries</h2>
+            <p>
+              In the world of JavaScript, there are various frameworks and
+              libraries that simplify and enhance the development process.
+              React.js is a popular JavaScript library for building user
+              interfaces, and Next.js is a framework built on top of React,
+              providing additional features like server-side rendering and
+              routing (this app is built with Next.js for example).
+            </p>
+            <div className={style.centeredImage}>
+              <Image
+                src="/_img/chapter4/nextjs.png"
+                alt="React and Next.js logos"
+                width={400}
+                height={200}
+              />
+            </div>
+          </section>
+
+          <section className={style.section}>
+            <h2>Additional Concepts</h2>
+            <p>
+              As you delve deeper into JavaScript, you'll encounter more
+              advanced concepts such as asynchronous programming, promises, and
+              the use of APIs to fetch and send data. These concepts are crucial
+              for building modern and responsive web applications.
+            </p>
+          </section>
+        </div>
+      </div>
+    </div>
+  );
+}
--- my-app/src/app/courses/javascript/chapter4/page.js
+++ my-app/src/app/courses/javascript/chapter4/page.js
@@ -1,15 +1,18 @@
-import FooterFixed from "@/src/app/_components/_footerFixed/FooterFixed";
+import Chapter4 from "@/src/app/_components/_chapter4/Chapter4";
+import Footer from "@/src/app/_components/_footer/Footer";
 import Navbar from "@/src/app/_components/_navbar/Navbar";
 import NextLesson from "@/src/app/_components/_nextLesson/NextLesson";
+import Pad from "@/src/app/_components/_pad/Pad";
 
 export default function Page() {
   const currentChapter = 4;
   return (
     <div>
       <Navbar></Navbar>
-      <h1>Chapter 4</h1>
+      <Chapter4></Chapter4>
       <NextLesson currentChapter={currentChapter}></NextLesson>
-      <FooterFixed></FooterFixed>
+      <Pad></Pad>
+      <Footer></Footer>
     </div>
-  )
+  );
 }
