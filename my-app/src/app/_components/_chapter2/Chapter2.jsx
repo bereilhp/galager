201c59313a1bc10487e250a9e4fdab9bb6f46acd
PedroB <bereilhp@gmail.com>
2023-11-11 18:41:41 +0100

    Chapter 2 Component

--- my-app/src/app/_components/_chapter2/Chapter2.jsx
+++ my-app/src/app/_components/_chapter2/Chapter2.jsx
@@ -9,90 +9,31 @@ export default function Chapter2() {
       <div className={style.content}>
         <div className={style.text}>
           <section className={style.section}>
-            <h2>The DOM (Document Object Model)</h2>
+            <h2>Variables and Declaration Again</h2>
             <p>
-              The DOM, or Document Object Model, is a programming interface for
-              web documents. It represents the structure of a document as a tree
-              of objects, where each object corresponds to a part of the
-              document, such as elements, attributes, and text. The DOM provides
-              a way for programs (like JavaScript) to manipulate the structure,
-              style, and content of web documents.
-            </p>
-            <div className={style.centeredImage}>
-              <Image
-                src="/_img/chapter1/dom.png"
-                alt="DOM image"
-                width={320}
-                height={200}
-              ></Image>
-            </div>
-          </section>
-
-          <section className={style.section}>
-            <h2>Comments in JavaScript</h2>
-            <p>
-              Comments are used to add explanatory notes to your code. In
-              JavaScript, you can use single-line comments with // or multi-line
-              comments between /* and */.
-            </p>
-            <div className={style.centeredImage}>
-              <Image
-                src="/_img/chapter1/comment.png"
-                alt="Comment example"
-                width={400}
-                height={300}
-              ></Image>
-            </div>
-          </section>
-
-          <section className={style.section}>
-            <h2>Variables and Declaration</h2>
-            <p>
-              Variables are used to store and manipulate data. You can declare
-              variables using <code>let</code> or <code>const</code>.
+              Variables in JavaScript are used to store and manipulate data.
+              Good practices include using <code>let</code> for variables that
+              may be reassigned, and <code>const</code> for variables that
+              should remain constant.
             </p>
 
-            <h4>let</h4>
+            <h4>Block Scope</h4>
             <p>
-              The <code>let</code> keyword is used to declare a variable with
-              block scope. This means that the variable is only accessible
-              within the block of code where it is defined, and it is not
-              visible outside of that block.
+              <code>let</code> and <code>const</code> have block scope, meaning
+              they are only accessible within the block of code where they are
+              defined.
             </p>
 
+            <h4>Naming Conventions</h4>
             <p>
-              <code>let</code> can be reassigned within the same block or scope,
-              but they cannot be redeclared in the same scope.{" "}
+              Follow good naming conventions, use descriptive names, and avoid
+              starting variable names with numbers.
             </p>
 
             <div className={style.centeredImage}>
               <Image
-                src="/_img/chapter1/let.png"
-                alt="let variable example"
-                width={400}
-                height={300}
-              ></Image>
-            </div>
-
-            <h4>const</h4>
-            <p>
-              The <code>const</code> keyword is used to declare a variable with
-              block scope, just like <code>let</code>. However, variables
-              declared with
-              <code>const</code> cannot be reassigned to a new value after their
-              initial assignment.
-            </p>
-
-            <p>
-              This means that once a value is assigned to a <code>const</code>{" "}
-              variable, it cannot be changed. It provides a way to create
-              variables with a fixed value throughout their scope.
-            </p>
-
-            <div className={style.centeredImage}>
-              <Image
-                src="/_img/chapter1/const.png"
-                alt="const variable example"
+                src="/_img/chapter2/let.png"
+                alt="good variables name example"
                 width={400}
                 height={250}
               ></Image>
@@ -100,104 +41,217 @@ export default function Chapter2() {
           </section>
 
           <section className={style.section}>
-            <h2>== vs ===</h2>
+            <h2>parseInt()</h2>
             <p>
-              In JavaScript, <code>==</code> and <code>===</code> are comparison
-              operators used to compare values. However, they behave differently
-              in terms of type coercion (automatic or implicit conversion of one
-              data type to another).
-            </p>
-            <h4>==</h4>
-            <p>
-              The <code>==</code> also called "equality operator" or "loose
-              equality operator" performs type coercion, allowing implicit
-              conversion of operand types before making the comparison. Only the
-              value the operands must be the same for <code>==</code> to return{" "}
-              <code>true</code>.
+              The <code>parseInt()</code> function in JavaScript is used to
+              parse a string and convert it to an integer. It takes two
+              parameters: the string to be converted and an optional radix
+              (base) for the numerical system.
             </p>
 
             <div className={style.centeredImage}>
               <Image
-                src="/_img/chapter1/equality.png"
-                alt="equality example"
-                width={400}
-                height={250}
-              ></Image>
-            </div>
-            <h4>===</h4>
-            <p>
-              On the other hand, the <code>===</code> also called "strict
-              equality operator" or simply the "strict equality comparator"
-              compares values without type coercion. Both the value and the type
-              of the operands must be the same for <code>===</code> to return{" "}
-              <code>true</code>.
-            </p>
-            <div className={style.centeredImage}>
-              <Image
-                src="/_img/chapter1/strictEquality.png"
-                alt="strict equality example"
-                width={400}
-                height={250}
+                src="/_img/chapter2/parseInt.png"
+                alt="parseInt example"
+                width={650}
+                height={310}
               ></Image>
             </div>
           </section>
 
           <section className={style.section}>
-            <h2>console.log()</h2>
+            <h2>Functions</h2>
             <p>
-              <code>console.log()</code> is used for logging messages to the
-              console. It's a helpful tool for debugging and understanding the
-              flow of your code.
+              Functions in JavaScript are blocks of code designed to perform a
+              specific task. They can be defined using the traditional function
+              declaration or the modern arrow function syntax.
             </p>
+
             <div className={style.centeredImage}>
               <Image
-                src="/_img/chapter1/consoleLog.png"
-                alt="console.log example"
-                width={400}
+                src="/_img/chapter2/func.png"
+                alt="function example"
+                width={500}
+                height={300}
+              ></Image>
+            </div>
+
+            <h4>Return</h4>
+
+            <p>
+              In JavaScript, the <code>return</code> statement is used in a
+              function to specify the value that the function should produce or
+              result in. When a function is called, the <code>return</code>{" "}
+              statement is executed, and the specified value is sent back to the
+              caller.
+            </p>
+
+            <h4>Arrow Functions</h4>
+            <p>
+              Arrow functions provide a concise syntax, especially useful for
+              short, simple functions. They inherit the <code>this</code> value
+              from the enclosing scope.
+            </p>
+
+            <div className={style.centeredImage}>
+              <Image
+                src="/_img/chapter2/arrow.png"
+                alt="arrow function example"
+                width={600}
                 height={250}
+              ></Image>
+            </div>
+
+            <h4>The "this" Keyword</h4>
+            <p>
+              The <code>this</code> keyword refers to the context of the current
+              function.
+            </p>
+          </section>
+
+          <section className={style.section}>
+            <h2>Arrays</h2>
+            <p>
+              Arrays in JavaScript are used to store multiple values in a single
+              variable. They are created using square brackets and can hold
+              various data types.
+            </p>
+
+            <div className={style.centeredImage}>
+              <Image
+                src="/_img/chapter2/array.png"
+                alt="array example"
+                width={600}
+                height={270}
               ></Image>
             </div>
           </section>
 
           <section className={style.section}>
-            <h2>String Functions</h2>
+            <h2>Data Types</h2>
             <p>
-              JavaScript provides built-in functions for manipulating strings,
-              such as <code>toLowerCase()</code> and <code>toUpperCase()</code>.
+              Simple data types, also known as primitive data types, are the
+              basic building blocks for representing and manipulating data in
+              programming languages. These data types are fundamental and
+              directly operate on the raw values. In JavaScript, some common
+              simple data types include:
             </p>
 
-            <h4>toLowerCase()</h4>
+            <h4>Numbers</h4>
             <p>
-              The <code>toLowerCase()</code> method in JavaScript is used to
-              convert all the characters in a string to lowercase. When applied
-              to a string, it returns a new string with all the alphabetic
-              characters in lowercase while leaving non-alphabetic characters
-              unchanged.
+              Represents numeric values (integers or floating-point numbers).
             </p>
 
             <div className={style.centeredImage}>
               <Image
-                src="/_img/chapter1/lower.png"
-                alt="console.log example"
-                width={750}
-                height={300}
+                src="/_img/chapter2/number.png"
+                alt="number example"
+                width={600}
+                height={270}
               ></Image>
             </div>
 
-            <h4>toUpperCase()</h4>
-            <p>
-              The <code>toUpperCase()</code> method in JavaScript is employed to
-              convert all the characters in a string to uppercase. When applied
-              to a string, it returns a new string with all the alphabetic
-              characters transformed to uppercase, while leaving non-alphabetic
-              characters unaffected.
-            </p>
+            <h4>Strings</h4>
+            <p>Represents sequences of characters (text).</p>
+
             <div className={style.centeredImage}>
               <Image
-                src="/_img/chapter1/upper.png"
-                alt="console.log example"
-                width={750}
-                height={300}
+                src="/_img/chapter2/string.png"
+                alt="string example"
+                width={600}
+                height={270}
+              ></Image>
+            </div>
+
+            <h4>Boolean</h4>
+            <p>Represents true or false values.</p>
+
+            <div className={style.centeredImage}>
+              <Image
+                src="/_img/chapter2/boolean.png"
+                alt="boolean example"
+                width={600}
+                height={270}
+              ></Image>
+            </div>
+
+            <h4>Undefined</h4>
+            <p>
+              Represents a variable that has been declared but not assigned a
+              value.
+            </p>
+
+            <div className={style.centeredImage}>
+              <Image
+                src="/_img/chapter2/undefined.png"
+                alt="undefined example"
+                width={600}
+                height={270}
+              ></Image>
+            </div>
+
+            <h4>Null</h4>
+            <p>Represents the intentional absence of any object value.</p>
+
+            <div className={style.centeredImage}>
+              <Image
+                src="/_img/chapter2/null.png"
+                alt="null example"
+                width={600}
+                height={270}
+              ></Image>
+            </div>
+          </section>
+
+          <section className={style.section}>
+            <h2>Objects in JavaScript</h2>
+            <p>
+              Objects are used to store collections of data in key-value pairs.
+              They are similar to arrays but use property names instead of
+              indexes.
+            </p>
+
+            <div className={style.centeredImage}>
+              <Image
+                src="/_img/chapter2/obj.png"
+                alt="object example"
+                width={600}
+                height={350}
+              ></Image>
+            </div>
+          </section>
+
+          <section className={style.section}>
+            <h2>JSON (JavaScript Object Notation)</h2>
+            <p>
+              JSON is a lightweight data interchange format. It is easy for
+              humans to read and write and easy for machines to parse and
+              generate. JSON is used to represent structured data and is often
+              used with web APIs.
+            </p>
+
+            <div className={style.centeredImage}>
+              <Image
+                src="/_img/chapter2/json.png"
+                alt="json example"
+                width={650}
+                height={270}
+              ></Image>
+            </div>
+
+            <h4>JSON.stringify()</h4>
+            <p>
+              The <code>JSON.stringify()</code> method converts a JavaScript
+              object or value to a JSON string. This is useful for sending data
+              to a server or displaying JSON data.
+            </p>
+
+            <div className={style.centeredImage}>
+              <Image
+                src="/_img/chapter2/stringify.png"
+                alt="JSON.stringify example"
+                width={600}
+                height={400}
               ></Image>
             </div>
           </section>
@@ -205,11 +259,11 @@ export default function Chapter2() {
           <section className={style.section}>
             <h2>Train what you just learned</h2>
             <ul>
-              <Link href="/exercises/quiz/medium">
-                <li>Medium JavaScript Quiz</li>
+              <Link href="/exercises/quiz/hard">
+                <li>Hard JavaScript Quiz</li>
               </Link>
-              <Link href="/exercises/fillTheBlank/easy">
-                <li>Easy JavaScript Fill in the Blank</li>
+              <Link href="/exercises/fillTheBlank/medium">
+                <li>Medium JavaScript Fill in the Blank</li>
               </Link>
             </ul>
           </section>
